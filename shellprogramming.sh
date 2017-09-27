21d255791692a86cb3f6fc6cae478c535fbd1733
dcr1586@inbox.lv <serjqab@gmail.com>
2017-09-27 11:46:53 +0300

    20170927_11_50

--- shellprogramming.sh
+++ shellprogramming.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+#4. piemērs
+echo "Tiek pildīts skripts: "$0
+#echo $0
+#echo $n
+echo "Skriptam nodotu argumentu skaits: "$#
+echo "Skriptam nodoti argumenti (attēlošana/grupēšana 1):"$*
+echo "Skriptam nodoti argumenti (attēlošana/grupēsana 2):"$@
+#echo $?
+echo "Skriptam piešķirtais procesa numurs: "$$
+#echo $!
 
-NAME="V U"
+#3. piemērs
+#NAME="V U"
+#echo $NAME
+#unset NAME
 #echo $NAME
 
-
-
+#2. piemērs
+#NAME="V U"
 #readonly NAME
 #echo $NAME
 #NAME="S B"
 #echo $NAME
 
-echo $NAME
-unset NAME
-echo $NAME
-
+#1. piemērs
+#NAME="V U"
+#echo $NAME
