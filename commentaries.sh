e1c2201b476fe959a00ae25bda3fffba472d7dd5
Live session user <serjqab@gmail.com>
2017-10-07 13:43:54 +0000

    Mājas darbs 10/07/17

---
+++ commentaries.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Tiek pildīts skripts: "$0
+
+echo "[cat]: Failu lasīšana;"
+echo "[cd]: Staigāšana pa direktoriju- uz priekšu, uz atpakaļū;" 
+echo "[chmod]: Privilēģijas mainīšana;"
+echo "[echo]: Teksts;"
+echo "[echo $0]: Linux shell;"
+echo "[history]: Rada komandu/darbību vēsturi;"
+echo "[ls]: Rāda direktorijas failus;"
+echo "[mkdir]: Failu veidošana;"
+echo "[man]: Paskaidro, ko nozīme komandas (piemērs- man ls);"
+echo "[nano]: Failu modificēšana jeb skripta veidošana;"
+echo "[path]: Ceļš;"
+echo "[pwd]: Rāda direktoriju, kurā tagad atrodies;"
+echo "[rmdir]: Tukšo direktoriju dzēšana;"
+echo "[who / whoami]: Rāda, kas ir tagad ielogojies;"
+echo "[TAB / Bultiņas]: palīgpogas (palīdz ātrai komandu rakstīšanai)."
+
+
