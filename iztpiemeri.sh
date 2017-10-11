a25841b231dce416ed4e41d2a563d7e6d3c6a529
dcr1586@inbox.lv <serjqab@gmail.com>
2017-10-11 11:49:50 +0300

    111017-11-50

--- iztpiemeri.sh
+++ iztpiemeri.sh
@@ -1,7 +1,86 @@
 #!/bin/bash
 
-#3.piemērs - operācijas ar mainīgājiem
+#5.piemērs
+: <<'END'
+a=7877680
+b=20989678
 
+if [ $a -eq $b ]
+then
+   echo "$a -eq $b : a is equal to b"
+else
+   echo "$a -eq $b: a is not equal to b"
+fi
+
+if [ $a -ne $b ]
+then
+   echo "$a -ne $b: a is not equal to b"
+else
+   echo "$a -ne $b : a is equal to b"
+fi
+
+if [ $a -gt $b ]
+then
+   echo "$a -gt $b: a is greater than b"
+else
+   echo "$a -gt $b: a is not greater than b"
+fi
+
+if [ $a -lt $b ]
+then
+   echo "$a -lt $b: a is less than b"
+else
+   echo "$a -lt $b: a is not less than b"
+fi
+
+if [ $a -ge $b ]
+then
+   echo "$a -ge $b: a is greater or  equal to b"
+else
+   echo "$a -ge $b: a is not greater or equal to b"
+fi
+
+if [ $a -le $b ]
+then
+   echo "$a -le $b: a is less or  equal to b"
+else
+   echo "$a -le $b: a is not less or equal to b"
+fi
+END
+
+
+
+
+#4.piemērs - operācijas ar argumentiem
+
+
+if [ $# == 2 ]
+# ja (if) skriptam nodotu argumentu skaits ($#)  ir vienads (==) ar 2 
+then
+# tad (then) izpildam sekojošās darbībaba lidz .........
+a=$1
+b=$2
+
+val41=`expr $a + $b`
+echo "$b + $a = "$val41
+
+val42=`expr $a - $b`
+echo "$a - $b = "$val42
+
+val43=`expr $a \* $b`
+echo "$a * $b = "$val43
+
+val44=`expr $a / $b`
+echo "$a / $b = "$val44
+
+val45=`expr $a % $b`
+echo "$a % $b = "$val45
+# ........................................  šai vietai
+fi
+
+
+#3.piemērs - operācijas ar mainīgājiem
+: <<'END'
 a=29
 b=15
 
@@ -19,7 +98,7 @@ echo "$a / $b = "$val34
 
 val35=`expr $a % $b`
 echo "$a % $b = "$val35
-
+END
 
 #2. piemērs - operācijas ar konstantēm
 : <<'END'
